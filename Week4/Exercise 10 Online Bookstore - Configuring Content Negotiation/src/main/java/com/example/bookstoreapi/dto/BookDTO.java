9c6c311ae67ff445bc17c7d645532d552722cce6
Akash Dey <144844103+akashdey29@users.noreply.github.com>
2024-08-21 14:38:39 +0530

    Add files via upload

---
+++ Week4/Exercise 10 Online Bookstore - Configuring Content Negotiation/src/main/java/com/example/bookstoreapi/dto/BookDTO.java
@@ -0,0 +1,12 @@
+package com.example.bookstoreapi.dto;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class BookDTO extends RepresentationModel<BookDTO> {
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+    private String isbn;
+    // Getters and Setters
+}
